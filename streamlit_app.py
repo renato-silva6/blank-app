b4a155353964ff52aaabfbe7b6cbfdd4d9db1eda
renato-silva6 <55024922+renato-silva6@users.noreply.github.com>
2024-11-26 21:37:34 +0000

    Added some page elements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.header("🎈 My new app")
+st.subheader("This is my subtitle")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.button("Click me")
+st.checkbox("I agree")
+st.pills("Tags", ["Sports", "Politics"])
+st.radio("Pick one", ["cats", "dogs"])
